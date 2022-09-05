92150d5c4dc47eed040783190f87ada2a2c0e0da
Sxeigumen <krasnov619@yandex.ru>
2022-09-05 12:38:55 +0300

    2

--- CSh_lab01/Program.cs
+++ CSh_lab01/Program.cs
@@ -10,7 +10,34 @@ namespace CSh_lab01
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("xddd");
+            Console.WriteLine("Type System.Boolean:    Min value = {0}    Max value = {1}", System.Boolean.FalseString, System.Boolean.TrueString);
+
+            Console.WriteLine("Type System.Char:       Min value = {0}    Max value = {1}", System.Char.MinValue, System.Char.MaxValue);
+
+            Console.WriteLine("Type System.Decimal:    Min value = {0}    Max value = {1}", System.Decimal.MinValue, System.Decimal.MaxValue);
+
+            Console.WriteLine("Type System.DateTime:   Min value = {0}    Max value = {1}", System.DateTime.MinValue, System.DateTime.MaxValue);
+
+            Console.WriteLine("Type System.Double:     Min value = {0}    Max value = {1}", System.Double.MinValue, System.Double.MaxValue);
+
+            Console.WriteLine("Type System.Single:     Min value = {0}    Max value = {1}", System.Single.MinValue, System.Single.MaxValue);
+
+            Console.WriteLine("Type System.SByte:      Min value = {0}    Max value = {1}", System.SByte.MinValue, System.SByte.MaxValue);
+
+            Console.WriteLine("Type System.Byte:       Min value = {0}    Max value = {1}", System.Byte.MinValue, System.Byte.MaxValue);
+
+            Console.WriteLine("Type System.Int16:      Min value = {0}    Max value = {1}", System.Int16.MinValue, System.Int16.MaxValue);
+
+            Console.WriteLine("Type System.Int32:      Min value = {0}    Max value = {1}", System.Int32.MinValue, System.Int32.MaxValue);
+
+            Console.WriteLine("Type System.Int64:      Min value = {0}    Max value = {1}", System.Int64.MinValue, System.Int64.MaxValue);
+
+            Console.WriteLine("Type System.UInt16:     Min value = {0}    Max value = {1}", System.UInt16.MinValue, System.UInt16.MaxValue);
+
+            Console.WriteLine("Type System.UInt32:     Min value = {0}    Max value = {1}", System.UInt32.MinValue, System.UInt32.MaxValue);
+
+            Console.WriteLine("Type System.UInt64:     Min value = {0}    Max value = {1}", System.UInt64.MinValue, System.UInt64.MaxValue);
+
         }
     }
 }
