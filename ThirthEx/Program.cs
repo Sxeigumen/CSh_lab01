5f0f22814ec47b82d7a68d1f88e6e175c1769c9b
Sxeigumen <krasnov619@yandex.ru>
2022-09-05 23:33:03 +0300

    Ex3 noat all func

---
+++ ThirthEx/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirthEx
+{
+    public class Point
+    {
+        public Point(int x, int y, string n)
+        {
+            coordinateX = x;
+            coordinateY = y;
+            figureName = n;
+        }
+        public int getX
+        {
+            get
+            {
+                return coordinateX;
+            }
+        }
+
+        public int getY
+        {
+            get
+            {
+                return coordinateY;
+            }
+        }
+
+        public string getName
+        {
+            get
+            {
+                return figureName;
+            }
+        }
+
+        public int coordinateX;
+        public int coordinateY;
+        public string figureName = "";
+    }
+
+    public class Figure
+    {
+        public Figure(Point p01, Point p02, Point p03, Point p04, Point p05)
+        {
+            p1 = p01;
+            p2 = p02;
+            p3 = p03;
+            p4 = p04;
+            p5 = p05;
+        }
+
+        public Figure(Point p01, Point p02, Point p03, Point p04)
+        {
+            p1 = p01;
+            p2 = p02;
+            p3 = p03;
+            p4 = p04;
+        }
+
+        public Figure(Point p01, Point p02, Point p03)
+        {
+            p1 = p01;
+            p2 = p02;
+            p3 = p03;
+        }
+
+        public double LenghtSide(Point A, Point B)
+        {
+            return Math.Sqrt(Math.Pow(A.coordinateX - B.coordinateX, 2) + Math.Pow(A.coordinateY - B.coordinateY, 2));
+        }
+        /*
+        public double PerimetrCalculator()
+        {
+            if(p4.figureName == "")
+            {
+                return LenghtSide(p1, p2) + LenghtSide(p2, p3) + LenghtSide(p3, p1);
+            }
+            else if(p5.figureName == "" && p4.figureName != "")
+            {
+                return LenghtSide(p1, p2) + LenghtSide(p2, p3) + LenghtSide(p3, p4) + LenghtSide(p4, p1);
+            }
+            return LenghtSide(p1, p2) + LenghtSide(p2, p3) + LenghtSide(p3, p4) + LenghtSide(p4, p5) + LenghtSide(p5, p1);
+
+        }
+        */
+        public Point p1;
+        public Point p2;
+        public Point p3;
+        public Point p4;
+        public Point p5;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(0, 0, "quad");
+            Point p2 = new Point(0, 5, "quad");
+            Point p3 = new Point(5, 5, "quad");
+            Point p4 = new Point(5, 0, "quad");
+
+            Figure fig1 = new Figure(p1, p2, p3, p4);
+
+            Console.WriteLine(fig1.LenghtSide(fig1.p1, fig1.p2));
+            Console.WriteLine(fig1.LenghtSide(fig1.p2, fig1.p3));
+            Console.WriteLine(fig1.LenghtSide(fig1.p3, fig1.p4));
+            Console.WriteLine(fig1.LenghtSide(fig1.p4, fig1.p1));
+            //Console.WriteLine(fig1.PerimetrCalculator());
+        }
+    }
+}
---
+++ ThirthEx/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ThirthEx")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThirthEx")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("42a08b62-6cf9-495b-84fb-9de2c413962b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
